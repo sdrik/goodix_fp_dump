37be8268ef3a8e6778c7109bd9b39776153e8d3c
Cédric Schieli <cschieli@gmail.com>
2019-10-06 21:17:04 +0200

    New device id 27c6:538c

--- goodix_fp_dump.c
+++ goodix_fp_dump.c
@@ -94,6 +94,15 @@ static const struct goodix_fp_usb_device_descriptor supported_devices[] = {
 		.output_endpoint = 0x01,
 		.input_endpoint = 0x83,
 	},
+	{
+		/* found on Dell Inspiron 7786 (2019) */
+		.vendor_id = 0x27c6,
+		.product_id = 0x538c,
+		.configuration = 1,
+		.output_endpoint = 0x01,
+		.input_endpoint = 0x83,
+       },
+
 };
 
 struct _goodix_fp_device {
